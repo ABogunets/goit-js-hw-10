92f42e3c7f07b3d7786ae3b1484145aeed052f39
ABogunets <abogunets@gmail.com>
2023-01-31 00:26:18 +0200

    Almost done
    stopped on handbars usage

--- src/index.js
+++ src/index.js
@@ -1,50 +1,87 @@
 import './css/styles.css';
-
-const DEBOUNCE_DELAY = 300;
-
-const ENDPOINT = "restcountries.com/v3.1/name/";
-
-
-// https://restcountries.com/v2/all?fields=name,capital,currencies
-// https://restcountries.com/v3.1/name/{name}
-
-// const inputRef = document.getElementById("search-box");
-// console.log('inputRef :>> ', inputRef);
-
-// function fetchCountries(name) {
-// fetch(`${ENDPOINT}'${name}?fields=name.official,capital,population,flags.svg,languages`)
-//   .then(response => response.json())
-//   .then(card => {
-//     console.log('card :>> ', card);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
-// }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import countryInfoTpl from './templates/country-info.hbs';
+import countryListTpl from './templates/country-list.hbs';
+import API from "./fetchCountries.js";
 
 
+const DEBOUNCE_DELAY = 700;
+const inputRef = document.getElementById("search-box");
 
+inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
+function onInput(e) {
+  const countryName = e.target.value.trim();
+  console.log(countryName);
+  if (countryName === '') {
+    clearCountryList();
+    return;
+  }
+  API.fetchCountries(countryName)
+    .then(countryCards => {
+    console.log('countryCards :>> ', countryCards);
 
+    const numberOfCountries = countryCards.length;
+    console.log('numberOfCountries :>> ', numberOfCountries);
 
-/*
-name.official - повна назва країни
-capital - столиця
-population - населення
-flags.svg - посилання на зображення прапора
-languages - масив мов
-*/
-/*
-function createMarkup({ name.official, capital, population, flags.svg, languages }) {
+    // if (numberOfCountries === 0) throw new Error("No data");
+    if (numberOfCountries > 10) Notify.info("Too many matches found. Please enter a more specific name.");
+    else if (numberOfCountries >= 2 && numberOfCountries <= 10) {
+      clearCountryList();
+      const countryListMarkup = countryCards.reduce(
+        (markup, card) => createCountryListMarkup(card) + markup, ""
+      );
+      updateCountryList(countryListMarkup);
+    }
+    else if (numberOfCountries === 1) {
+      clearCountryList();
+      const countryInfoMarkup = countryCards.reduce(
+        (markup, card) => createCountryInfoMarkup(card) + markup, ""
+      );
+      console.log('countryInfoMarkup :>> ', countryInfoMarkup);
+      updateCountryInfo(countryInfoMarkup);
+      console.log('countryInfoTpl :>> ', countryInfoTpl(countryCards[0]));
+    }
+    }) 
+  .catch(onFetchError);
+}
+
+function createCountryListMarkup({name, flags}) {
   return `
-    <div class="article-card">
-         <img src=${urlToImage} class="article-img">
-        <h2 class="article-title">${title}</h2>
-        <h3 class="article-author">${author || "Anonym"}</h3>
-        <p class="article-description">${description}</p>
-        <a href=${url} class="article-link" target="_blank">Read more</a>
-    </div>
-    
+      <li class="item">
+      <img class="flag" src="${flags.svg}" alt="flag" width="20">
+        ${name.common}
+      </li>
+      `;
+}
+
+function createCountryInfoMarkup({name, capital, population, flags, languages }) {
+  return `
+      <h1 class="country-title">
+        <img class="flag" src="${flags.svg}" alt="flag" width="30">
+        ${name.official}
+      </h1>
+      <p class="parameter"><b>Capital: </b>${capital}</p>
+      <p class="parameter"><b>Population: </b>${population}</p>
+      <p class="parameter"><b>Languages: </b>${Object.values(languages)}</p>
     `;
 }
-*/
+function updateCountryInfo(markup) {
+  document
+    .querySelector(".country-info")
+    .innerHTML = markup;
+}
+function updateCountryList(markup) {
+  document
+    .querySelector(".country-list")
+    .innerHTML = markup;
+}
+function clearCountryList() {
+  document.querySelector(".country-list").innerHTML = "";
+  document.querySelector(".country-info").innerHTML = "";
+}
+
+function onFetchError(error) {
+  Notify.failure("Oops, there is no country with that name")
+}
