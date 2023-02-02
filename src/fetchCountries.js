659e220548da946202750d4af10f509422c84ffa
ABogunets <abogunets@gmail.com>
2023-02-02 20:02:38 +0200

    refactored to async

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,12 @@
 const ENDPOINT = "https://restcountries.com/v3.1/name/";
 
-function fetchCountries(name) {
-  return fetch(`${ENDPOINT}${name}?fields=name,capital,population,flags,languages`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-}
+async function fetchCountries(name) {
+  const response = await fetch(`${ENDPOINT}${ name }?fields = name, capital, population, flags, languages`);
+  if (!response.ok) {
+  throw new Error(response.status);
+  }
+  const country = await response.json();
+  return country
+  }
 
 export default { fetchCountries };
